2e201ec6026e6a586afc586c1ff81c0ac9749b9c
Patrick Barker <patrickbarkerco@gmail.com>
2023-03-21 14:14:00 -0600

    dep finder working

---
+++ modelos/util/deps/__init__.py
@@ -0,0 +1,1 @@
+from .reqs import get_deps_for_type, format_imports  # noqa
--- modelos/util/deps/reqs.py
+++ modelos/util/deps/reqs.py
@@ -1,13 +1,29 @@
-from typing import Type, List, Union, Set, Tuple
+from typing import Type, List, Union, Set, Optional, Dict
 import inspect
+import logging
 from pathlib import Path
 import ast
 import os
 from importlib.machinery import ModuleSpec
 from importlib.util import find_spec
-from importlib.metadata import version
+
+from pipreqs.pipreqs import (
+    get_pkg_names,
+    get_import_local,
+    get_imports_info,
+)
+
 
 from modelos.project import Project
+
+
+def _clean_mod_name(name: str) -> str:
+    cleaned_name, _, _ = name.partition(".")
+    return cleaned_name
+
+
+def _join(f):
+    return os.path.join(os.path.dirname(__file__), f)
 
 
 def find_imports(fp: str) -> List[Union[ast.Import, ast.ImportFrom]]:
@@ -41,7 +57,15 @@ def is_stdlib(spec: ModuleSpec) -> bool:
     Returns:
         bool: Whether it is in the stdlib
     """
-    parts = spec.name.split(".")
+
+    name = _clean_mod_name(spec.name)
+    parts = name.split(".")
+
+    with open(_join("stdlib"), "r") as f:
+        data = {x.strip() for x in f}
+        if parts[0] in data:
+            return True
+
     if len(parts) > 1:
         _spec = find_spec(parts[0])
         if _spec is None:
@@ -136,11 +160,21 @@ def _mods_to_search(spec: ModuleSpec) -> List[ModuleSpec]:
 
 # TODO: Another way to do this may be to launch a subprocess and check the globals there
 # would need to use spawn instead of fork
-def get_deps_for_type(t: Type) -> List[Tuple[str, str]]:
+def get_deps_for_type(
+    t: Type,
+    proxy: Optional[str] = None,
+    local_only: bool = False,
+    pypi_server: str = "https://pypi.python.org/pypi/",
+    encoding: Optional[str] = None,
+) -> List[Dict[str, str]]:
     """Get all external dependencies for the given type
 
     Args:
         t (Type): The type to check
+        proxy (Optional[str], optional): Proxy to use. Defaults to None.
+        local_only (bool, optional): Whether to only look locally. Defaults to False.
+        pypi_server (str, optional): Pypi server to use. Defaults to "https://pypi.python.org/pypi/".
+        encoding (Optional[str], optional): File encoding to use when reading. Defaults to None.
 
     Returns:
         List[Tuple[str, str]]: A list of dep to version
@@ -151,21 +185,47 @@ def get_deps_for_type(t: Type) -> List[Tuple[str, str]]:
 
     fp = inspect.getfile(t)
 
-    fin: Set[Tuple[str, str]] = set()
+    fin: Dict[str, Dict[str, str]] = {}
     found: Set[str] = set()
 
-    def add(current: ModuleSpec, found: ModuleSpec):
+    def _get_pkg_info(spec: ModuleSpec) -> Dict[str, str]:
+        nonlocal proxy, local_only, encoding
+
+        mod_name = _clean_mod_name(spec.name)
+        candidates = get_pkg_names([mod_name])
+
+        use_proxy = None
+        if proxy:
+            use_proxy = {"http": proxy, "https": proxy}
+
+        if local_only:
+            logging.debug("Getting package information ONLY from local installation.")
+            imports = get_import_local(candidates, encoding=encoding)
+        else:
+            logging.debug("Getting packages information from Local/PyPI")
+            local = get_import_local(candidates, encoding=encoding)
+            # Get packages that were not found locally
+            difference = [x for x in candidates if x.lower() not in [z["name"].lower() for z in local]]
+            imports = local + get_imports_info(difference, proxy=use_proxy, pypi_server=pypi_server)
+        # sort imports based on lowercase name of package, similar to `pip freeze`.
+        # imports = sorted(imports, key=lambda x: x["name"].lower())
+
+        return imports[0]
+
+    def _add(current: ModuleSpec, found: ModuleSpec):
         nonlocal fin
 
         if _should_add(current, found):
-            base_name = found.name.split(".")[0]
-
-            ver = version(base_name)
-            fin.add((base_name, ver))
+            info = _get_pkg_info(found)
+            fin[info["name"]] = info
 
     def _find_in_spec(spec: ModuleSpec):
         nonlocal fin
         imports = find_imports(fp)
+
+        if spec.origin is None:
+            logging.warning(f"spec origin was none: {spec.name}")
+            return
 
         for imp in imports:
             if isinstance(imp, ast.ImportFrom):
@@ -181,7 +241,7 @@ def get_deps_for_type(t: Type) -> List[Tuple[str, str]]:
                         continue
                     found.add(_spec.name)
 
-                    add(spec, _spec)
+                    _add(spec, _spec)
                     if _should_search(spec, _spec):
                         for s in _mods_to_search(_spec):
                             _find_in_spec(s)
@@ -195,7 +255,7 @@ def get_deps_for_type(t: Type) -> List[Tuple[str, str]]:
                         continue
                     found.add(_spec.name)
 
-                    add(spec, _spec)
+                    _add(spec, _spec)
                     if _should_search(spec, _spec):
                         for s in _mods_to_search(_spec):
                             _find_in_spec(s)
@@ -209,7 +269,7 @@ def get_deps_for_type(t: Type) -> List[Tuple[str, str]]:
                         continue
                     found.add(_spec.name)
 
-                    add(spec, _spec)
+                    _add(spec, _spec)
                     if _should_search(spec, _spec):
                         for s in _mods_to_search(_spec):
                             _find_in_spec(s)
@@ -219,4 +279,23 @@ def get_deps_for_type(t: Type) -> List[Tuple[str, str]]:
         raise ValueError("type given has no module spec")
     _find_in_spec(spec)
 
-    return list(fin)
+    out: List[Dict[str, str]] = []
+    for _, v in fin.items():
+        out.append(v)
+    return out
+
+
+def format_imports(imports: List[Dict[str, str]], symbol: str = "~=") -> str:
+    """Convert imports to a requirements.txt format
+
+    Args:
+        imports (List[Dict[str, str]]): Imports, should be generated from 'get_pkg_imports()'
+        symbol (str, optional): Symbol to use on joining name to version. Defaults to "~=".
+
+    Returns:
+        str: A stringified requirements file format
+    """
+    fmt = "{name}" + symbol + "{version}"
+    lines = "\n".join(fmt.format(**item) if item["version"] else "{name}".format(**item) for item in imports) + "\n"
+
+    return lines
